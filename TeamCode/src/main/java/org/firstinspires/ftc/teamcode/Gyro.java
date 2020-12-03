f160c2a0ecba43cb9f054ad650b335cafc57ab34
Simon <SimonNDao12@gmail.com>
2020-12-03 09:57:07 -0800

    added a function that displays heading data to the phone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -3,16 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class Gyro {
-    BNO055IMU gyro;
-    Orientation angles;
+    private BNO055IMU gyro;
+    private Orientation angles;
+    private Telemetry telemetry;
 
-    Gyro(HardwareMap hardwareMap) {
+    Gyro(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.mode = BNO055IMU.SensorMode.IMU;
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
@@ -29,4 +32,7 @@ public class Gyro {
         return angles.firstAngle;
     }
 
+    public void addGyroData() {
+        telemetry.addData("Heading = ", getDirection());
+    }
  }
