37a9eeaa51487d2ce005443c78c4daa332b159f3
Simon <SimonNDao12@gmail.com>
2021-01-26 18:11:29 -0800

    made a ChaseWobbleGoal and made and edited ChaseTowerGoal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaseWobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaseWobbleGoal.java
@@ -92,10 +92,10 @@ public class ChaseWobbleGoal extends LinearOpMode
 
         while (opModeIsActive())
         {
-            towerX = RingDeterminationPipeline.towerX;
-            towerY = RingDeterminationPipeline.towerY;
-            towerWidth = RingDeterminationPipeline.towerWidth;
-            towerHeight = RingDeterminationPipeline.towerHeight;
+            towerX = RingDeterminationPipeline.wobbleX;
+            towerY = RingDeterminationPipeline.wobbleY;
+            towerWidth = RingDeterminationPipeline.wobbleWidth;
+            towerHeight = RingDeterminationPipeline.wobbleHeight;
 
             double dw = targetWidth - towerWidth;
             double dx = targetX - towerX;
@@ -142,20 +142,10 @@ public class ChaseWobbleGoal extends LinearOpMode
     public static class RingDeterminationPipeline extends OpenCvPipeline
     {
         public Mat mask;
-        public static int towerX = 0;
-        public static int towerY = 0;
-        public static int towerWidth = 0;
-        public static int towerHeight = 0;
-
-        /*
-         * An enum to define the ring position
-         */
-        public enum RingPosition
-        {
-            FOUR,
-            ONE,
-            NONE
-        }
+        public static int wobbleX = 0;
+        public static int wobbleY = 0;
+        public static int wobbleWidth = 0;
+        public static int wobbleHeight = 0;
 
         /*
          * Some color constants
@@ -163,98 +153,31 @@ public class ChaseWobbleGoal extends LinearOpMode
         static final Scalar BLUE = new Scalar(0, 123, 0);
         static final Scalar GREEN = new Scalar(28, 255,880);
 
-        /*
-         * The core values which define the location and size of the sample regions
-         */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181,98);
-
-        static final int REGION_WIDTH = 35;
-        static final int REGION_HEIGHT = 25;
-
-        final int FOUR_RING_THRESHOLD = 150;
-        final int ONE_RING_THRESHOLD = 135;
-
-        Point region1_pointA = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x,
-                REGION1_TOPLEFT_ANCHOR_POINT.y);
-        Point region1_pointB = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-        /*
-         * Working variables
-         */
-
-        Mat region1_Cb;
-        Mat YCrCb = new Mat();
-        Mat Cb = new Mat();
-        int avg1;
-
-        // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile RingPosition position = RingPosition.FOUR;
-
-        /*
-         * This function takes the RGB frame, converts to YCrCb,
-         * and extracts the Cb channel to the 'Cb' variable
-         */
-        void inputToCb(Mat input)
-        {
-            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(YCrCb, Cb, 1);
-        }
 
         @Override
         public void init(Mat firstFrame)
         {
-            inputToCb(firstFrame);
-
-            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
         }
 
         @Override
         public Mat processFrame(Mat input)
         {
-            inputToCb(input);
-
-            avg1 = (int) Core.mean(region1_Cb).val[0];
-
-            /*
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
-             */
-            position = RingPosition.FOUR; // Record our analysis
-            if(avg1 > FOUR_RING_THRESHOLD){
-                position = RingPosition.FOUR;
-            } else if (avg1 > ONE_RING_THRESHOLD){
-                position = RingPosition.ONE;
-            } else {
-                position = RingPosition.NONE;
-            }
 
             //update ring coordinates
-            int[] coords = getRingCoordinates(input);
-            towerX = coords[0];
-            towerY = coords[1];
-            towerWidth = coords[2];
-            towerHeight = coords[3];
+            int[] coords = getWobbleCoordinates(input);
+            wobbleX = coords[0];
+            wobbleY = coords[1];
+            wobbleWidth = coords[2];
+            wobbleHeight = coords[3];
 
             Imgproc.rectangle(
                     input, // Buffer to draw on
-                    new Point(towerX,towerY), // First point which defines the rectangle
-                    new Point(towerX + towerWidth,towerY + towerHeight), // Second point which defines the rectangle
+                    new Point(wobbleX, wobbleY), // First point which defines the rectangle
+                    new Point(wobbleX + wobbleWidth, wobbleY + wobbleHeight), // Second point which defines the rectangle
                     GREEN, // The color the rectangle is drawn in
                     -1); // Negative thickness means solid fill
 
             return input;
-        }
-
-        public int getAnalysis()
-        {
-            return avg1;
         }
 
         public int[] getWobbleCoordinates(Mat input) {
@@ -286,12 +209,6 @@ public class ChaseWobbleGoal extends LinearOpMode
 
             for (int i = 0; i < contours.size(); i++) {
                 Rect rect = Imgproc.boundingRect(contours.get(i));
-
-                //dont draw a square around a spot that's too small
-                //to avoid false detections
-                if (rect.area() > 7_000) {
-                    Imgproc.rectangle(src, rect, GREEN, 5);
-                }
             }
 
             Rect largest = new Rect();
