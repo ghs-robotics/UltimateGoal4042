0ddb1e43f865d5e27e03927dc355ee9ba90db756
1kavennemann <slightlyenergized@gmail.com>
2021-01-28 18:09:38 -0800

    edits to RobotCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCV.java
@@ -69,7 +69,7 @@ class RobotCV {
 
     //CV objects
     OpenCvInternalCamera phoneCam;
-    RingDeterminationPipeline pipeline;
+    ObjectDeterminationPipeline pipeline;
 
     //Creates a robot object with methods that we can use in both Auto and TeleOp
     RobotCV(HardwareMap hardwareMap, Telemetry telemetry) {
@@ -106,7 +106,7 @@ class RobotCV {
         //initiating some CV variables/objects
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
-        pipeline = new RingDeterminationPipeline();
+        pipeline = new ObjectDeterminationPipeline();
         phoneCam.setPipeline(pipeline);
     }
 
@@ -264,7 +264,55 @@ class RobotCV {
         while (getElapsedTimeSeconds() - start < seconds) {}
     }
 
-    public static class RingDeterminationPipeline extends OpenCvPipeline
+    public static int[] getObjectCoordinates(Mat input, Scalar lower, Scalar upper) {
+        Scalar GREEN = new Scalar(0, 255, 0);
+
+        Mat dst = new Mat();
+        Mat src = input;
+        Imgproc.resize(src, src, new Size(320, 240));
+
+        Imgproc.rectangle(src, new Point(0,0), new Point(320, 0), GREEN, -1);
+
+        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2HSV);
+        Imgproc.GaussianBlur(dst, dst, new Size(5, 5), 80, 80);
+
+        //adding a mask to the dst mat
+        Core.inRange(dst, lower, upper, dst);
+
+        //dilate the ring to make it easier to detect
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+        Imgproc.dilate(dst, dst, kernel);
+
+        //get the contours of the ring
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(dst, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        //draw a contour on the src image
+        Imgproc.drawContours(src, contours, -1, GREEN, 2, Imgproc.LINE_8, hierarchy, 2, new Point());
+
+        for (int i = 0; i < contours.size(); i++) {
+            Rect rect = Imgproc.boundingRect(contours.get(i));
+
+            //don't draw a square around a spot that's too small
+            //to avoid false detections
+            if (rect.area() > 7_000) { Imgproc.rectangle(src, rect, GREEN, 5); }
+        }
+
+        Rect largest = new Rect();
+        for (int i = 0; i < contours.size(); i++) {
+            Rect rect = Imgproc.boundingRect(contours.get(i));
+
+            if (largest.area() < rect.area()) { largest = rect; }
+        }
+
+        //draws largest rect
+        Imgproc.rectangle(src, largest, GREEN, 5);
+
+        return new int[]{largest.x, largest.y, largest.width, largest.height};
+    }
+
+    public static class ObjectDeterminationPipeline extends OpenCvPipeline
     {
         //HSV constants
         public static final Scalar LOWER_RING_HSV = new Scalar(74, 153, 144);
@@ -278,7 +326,6 @@ class RobotCV {
         public static final int SCREEN_HEIGHT = 240;
         public static final int SCREEN_WIDTH = 320;
 
-        public Mat mask;
         public int objectX = 0;
         public int objectY = 0;
         public int objectWidth = 0;
@@ -291,7 +338,7 @@ class RobotCV {
         public Mat processFrame(Mat input)
         {
             //update ring coordinates
-            int[] coords = getObjectCoordinates(input, LOWER_RING_HSV, UPPER_RING_HSV);
+            int[] coords = RobotCV.getObjectCoordinates(input, LOWER_RING_HSV, UPPER_RING_HSV);
             objectX = coords[0];
             objectY = coords[1];
             objectWidth = coords[2];
@@ -305,54 +352,6 @@ class RobotCV {
                     -1); // Negative thickness means solid fill
 
             return input;
-        }
-
-        public int[] getObjectCoordinates(Mat input, Scalar lower, Scalar upper) {
-            Scalar GREEN = new Scalar(0, 255, 0);
-
-            Mat dst = new Mat();
-            Mat src = input;
-            Imgproc.resize(src, src, new Size(320, 240));
-
-            Imgproc.rectangle(src, new Point(0,0), new Point(320, 0), GREEN, -1);
-
-            Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2HSV);
-            Imgproc.GaussianBlur(dst, dst, new Size(5, 5), 80, 80);
-
-            //adding a mask to the dst mat
-            Core.inRange(dst, lower, upper, dst);
-
-            //dilate the ring to make it easier to detect
-            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
-            Imgproc.dilate(dst, dst, kernel);
-
-            //get the contours of the ring
-            List<MatOfPoint> contours = new ArrayList<>();
-            Mat hierarchy = new Mat();
-            Imgproc.findContours(dst, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-            //draw a contour on the src image
-            Imgproc.drawContours(src, contours, -1, GREEN, 2, Imgproc.LINE_8, hierarchy, 2, new Point());
-
-            for (int i = 0; i < contours.size(); i++) {
-                Rect rect = Imgproc.boundingRect(contours.get(i));
-
-                //don't draw a square around a spot that's too small
-                //to avoid false detections
-                if (rect.area() > 7_000) { Imgproc.rectangle(src, rect, GREEN, 5); }
-            }
-
-            Rect largest = new Rect();
-            for (int i = 0; i < contours.size(); i++) {
-                Rect rect = Imgproc.boundingRect(contours.get(i));
-
-                if (largest.area() < rect.area()) { largest = rect; }
-            }
-
-            //draws largest rect
-            Imgproc.rectangle(src, largest,GREEN, 5);
-
-            return new int[]{largest.x,largest.y, largest.width, largest.height};
         }
     }
 }
